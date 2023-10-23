d1fcd7f2d44be65f50f56167ecbcd46fb4a9881b
s-alad <n7.saad@gmail.com>
2023-10-23 18:32:59 -0400

    start layout

---
+++ dibs-app/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack, Tabs, router } from 'expo-router';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Appearance, Platform, TouchableOpacity, useColorScheme, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+
+export default function RootLayout() {
+
+
+  return (
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1, alignContent: 'center', justifyContent: 'center', alignItems: 'center' }}>  
+        <View>
+          <Text style={{color: 'black'}}>.</Text>
+        </View>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
+  )
+}
