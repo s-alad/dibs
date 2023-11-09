60abdfb17e3d592eb2a64c4b4379b7c97066da02
Raquel <raquelgr@bu.edu>
2023-11-09 12:55:11 -0500

    Added icons to Account

--- dibs-app/app/(protected)/account.tsx
+++ dibs-app/app/(protected)/account.tsx
@@ -10,14 +10,12 @@ export default function Account() {
     ]
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "white"} }>
-            
+            <Image source={require('dibs-app/assets/exit.png')} style ={{alignSelf:"flex-start", marginLeft:"5%"}} />
             <Image source={require('dibs-app/assets/Ellipse.png')} style = {{width:120, height:120, borderRadius: 200/2}}/>
-            <Text style={{fontSize:25, flex: 1, marginBottom: 50}}>User12345</Text>
-            
+            <Text style={{fontSize:25, marginBottom: 10}}>User12345</Text>
             <ScrollView
                 style={{
                     width: "100%",
-                    
                 }}
                 centerContent={true}
             >
@@ -26,6 +24,7 @@ export default function Account() {
                         <View style ={{
                             alignItems: "center"
                         }}>
+                        <Image source={require('dibs-app/assets/like.png')} style={{ alignSelf: "flex-end", marginRight: "6%"}}/>
                         <Listing/>
                         </View>
                     ))
