bc462710d9339ab8d6ec4d6a9bb52d3ffe981f33
Raquel <raquelgr@bu.edu>
2023-11-26 15:04:25 -0500

    profile pic UI

--- dibs-app/app/(auth)/onboard.tsx
+++ dibs-app/app/(auth)/onboard.tsx
@@ -1,10 +1,65 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Image, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Image, ActivityIndicator, Button, Pressable } from "react-native";
 
 export default function Onboard() {
     return (
-        <><View style={{backgroundColor: "black", height: 40, width: 40}}></View>
+        <>
+            <Text style={styles.text}>You're In!</Text>
+            <Text style={styles.subtext}>Select a profile photo:</Text>
+            <View style={styles.imageContainer}>
+                <View style = {styles.row}>
+                <Image source={require('dibs-app/assets/Ellipse1.png')} style={styles.image} />
+                <Image source={require('dibs-app/assets/Ellipse2.png')} style={styles.image} />
+                </View>
+                <View style = {styles.row}>
+                <Image source={require('dibs-app/assets/Ellipse3.png')} style={styles.image} />
+                <Image source={require('dibs-app/assets/Ellipse4.png')} style={styles.image} />
+                </View>
+            </View>
+            <Pressable style={styles.button}><Text style={styles.textButton}>CONTINUE</Text></Pressable>
+
             
         </>
     )
 }
+const styles = StyleSheet.create({
+    text:{
+        fontSize: 60,
+        textAlign: "center",
+        marginTop: "40%"
+    },
+    subtext:{
+        fontSize: 25,
+        textAlign: "center",
+        marginTop:"10%",
+       
+    },
+    imageContainer:{
+        display: "flex",
+        alignItems: "center",
+        marginTop:10
+    },
+    image:{
+        width: 120, height: 120, borderRadius: 200 / 2 
+     },
+     row:{
+        display:"flex",
+        flexDirection: "row",
+        margin: 15,
+        gap: 10
+
+     },
+     button: {
+        backgroundColor: "black",
+        height: 60,
+        width: "95%",
+        borderRadius: 30,
+        margin: 10,
+        marginTop: 35,
+        justifyContent: 'center',
+        alignItems:"center"
+     },
+     textButton:{
+        color: "white"
+     }
+})
---
+++ dibs-app/util/profilePictures.ts
@@ -0,0 +1,6 @@
+export const pictures = {
+    1: 'dibs-app/assets/Ellipse1.png',
+    2: 'dibs-app/assets/Ellipse2.png',
+    3:'dibs-app/assets/Ellipse3.png' ,
+    4:'dibs-app/assets/Ellipse4.png'
+}
