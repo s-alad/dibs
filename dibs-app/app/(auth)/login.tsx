90ed45ff7e2ae62058b62514d065808cef72e0f1
s-alad <n7.saad@gmail.com>
2023-12-13 19:29:20 -0500

    refact

--- dibs-app/app/(auth)/login.tsx
+++ dibs-app/app/(auth)/login.tsx
@@ -6,37 +6,31 @@ import * as WebBrowser from "expo-web-browser";
 WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
-    const { user, userLogin, authenticationStatus} = useAuthContext();
+    const { user, userLogin, authenticationStatus } = useAuthContext();
 
     return (
-        <View style={{ display: "flex", flex: 1, alignItems: "center", backgroundColor:"black"}}>
-            <View style={{height: "25%"}}></View> 
+        <View style={{ display: "flex", flex: 1, alignItems: "center", backgroundColor: "black" }}>
+            <View style={{ height: "25%" }}></View>
 
-            <View style={{width: "80%", alignItems:"center"}}>
-                <Image source={require("../../assets/bigLogo.png")} style={{width: 500, height: "50%", alignSelf:"center"}}/>
-                <TouchableOpacity onPress={
-                    () => {
-                        userLogin()
-                    }
-                }>
-                    <View 
+            <View style={{ width: "80%", alignItems: "center" }}>
+                <Image source={require("../../assets/bigLogo.png")} style={{ width: 500, height: "50%", alignSelf: "center" }} />
+                <TouchableOpacity onPress={() => { userLogin() }}>
+                    <View
                         style={{
                             backgroundColor: 'black',
                             width: 50,
                             height: 50,
-                            borderRadius: 50/2,
+                            borderRadius: 50 / 2,
                             justifyContent: "center",
                             alignItems: "center",
-                            borderColor:"grey",
-                            borderWidth:2,
+                            borderColor: "grey",
+                            borderWidth: 2,
                         }}
                     >
                         <Image
                             source={require('../../assets/google.png')}
-                            style={{
-                                width: 26,
-                                height: 26,
-                            }}/>
+                            style={{ width: 26, height: 26,}}
+                        />
                     </View>
                 </TouchableOpacity>
                 <Text
@@ -55,10 +49,9 @@ export default function Login() {
                             )
                         )
                     }
-                    {}
                 </Text>
-                <Text style={{textAlign:"center", marginHorizontal: 30, marginTop: 50, color:"white"}}>
-                This version is currently only available to <Text style={{fontWeight:"bold"}}>Boston University</Text> students.  
+                <Text style={{ textAlign: "center", marginHorizontal: 30, marginTop: 50, color: "white" }}>
+                    This version is currently only available to <Text style={{ fontWeight: "bold" }}>Boston University</Text> students.
                 </Text>
             </View>
         </View>
